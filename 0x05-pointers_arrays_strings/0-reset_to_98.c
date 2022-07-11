1c17a9629bf542ccb38faff9c3f3ea9303fee3a8
Kipropkevin <kipropkevin93@gmail.com>
2022-07-11 07:55:25 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /**
- * Reset_to_98 - takes a pointer to an int parameter
+ * reset_to_98 - takes a pointer to an int parameter
  *           and updates the value it points to 98
  *
  * @n: int parameter
