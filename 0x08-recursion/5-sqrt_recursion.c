a04bb84fccff541c2a93c7acf73257051ec1caae
Kipropkevin <kipropkevin93@gmail.com>
2022-07-20 14:01:38 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,25 @@
+#include "holberton.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
+ * is_squre_root - determines the square root of
+ * @j: an input value
+ * @i: input value
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
+ * Description: determines the square root
+ * Return: returns the input val
  */
-int _sqrt_recursion_wrapper(int n, int min, int max)
+
+int is_square_root(int i, int j)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
-
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
+	if (j * j > i)
+	{
 		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	}
+	if (j * j == i)
+	{
+		return (j);
+	}
+	return (is_square_root(i, j + 1));
 }
 
 /**
